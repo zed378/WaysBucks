ac3bd5807bf4b8bdb9db976caadb9d90730a9285
Muhammad Zawawi <kembedt@gmail.com>
2022-02-17 19:59:02 +0700

    update

--- client/src/pages/admin/ProductAdd.js
+++ client/src/pages/admin/ProductAdd.js
@@ -1,16 +1,23 @@
 // import package
 import { useState } from "react";
-
-// import component
+import { useNavigate } from "react-router-dom";
 
 // import assets
 import cssModules from "../../assets/css/ProductAdd.module.css";
 import clip from "../../assets/img/clip.svg";
 
+// import config
+import { API } from "../../config/api";
+
 function ProductAdd() {
+  const navigate = useNavigate();
   const pic = () => {
     document.getElementById("pic").click();
   };
+
+  // alert
+  const [success, setSuccess] = useState(false);
+  const [fail, setFail] = useState(false);
 
   // store data
   const [form, setForm] = useState({
@@ -21,10 +28,6 @@ function ProductAdd() {
 
   // set preview image
   const [preview, setPreview] = useState(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
 
   const handleChange = (e) => {
     setForm({
@@ -39,23 +42,98 @@ function ProductAdd() {
     }
   };
 
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+
+      const config = {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      };
+
+      // Store form data as object
+      const formData = new FormData();
+      formData.set("title", form.title);
+      formData.set("price", form.price);
+      formData.set("thumbnail", form.thumbnail[0], form.thumbnail[0].name);
+
+      const response = await API.post("/product", formData, config);
+
+      if (response.data.status === "Success") {
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(false);
+          navigate("/");
+        }, 3000);
+      } else if (response.data.status === "Failed") {
+        setFail(true);
+        setTimeout(() => {
+          setFail(false);
+        }, 3000);
+      }
+    } catch (error) {
+      setFail(true);
+      setTimeout(() => {
+        setFail(false);
+      }, 4000);
+    }
+  };
+
   return (
     <>
       <div className={cssModules.bodyProduct}>
         <div className={cssModules.formContainer}>
           <h1>Add Product</h1>
+
+          {success ? (
+            <h3
+              style={{
+                color: "green",
+                background: "#c5fce5",
+                textAlign: "center",
+                padding: "0.5rem 0",
+                fontSize: "1.15rem",
+                fontFamily: "avenirs",
+              }}
+            >
+              Add Product Success
+            </h3>
+          ) : (
+            <></>
+          )}
+
+          {fail ? (
+            <h3
+              style={{
+                color: "red",
+                background: "#e0cecc",
+                textAlign: "center",
+                padding: "0.5rem 0",
+                fontSize: "1.15rem",
+                fontFamily: "avenirs",
+              }}
+            >
+              Add Product Failed
+            </h3>
+          ) : (
+            <></>
+          )}
+
           <form onSubmit={handleSubmit}>
             <input
               type="text"
               name="title"
               placeholder="Product Name"
               onChange={handleChange}
+              required
             />
             <input
               type="number"
               name="price"
               placeholder="Price"
               onChange={handleChange}
+              required
             />
             <input
               type="file"
--- client/src/pages/admin/ToppingAdd.js
+++ client/src/pages/admin/ToppingAdd.js
@@ -1,16 +1,23 @@
 // import package
 import { useState } from "react";
-
-// import component
+import { useNavigate } from "react-router-dom";
 
 // import assets
 import cssModules from "../../assets/css/ToppingAdd.module.css";
 import clip from "../../assets/img/clip.svg";
 
+// import config
+import { API } from "../../config/api";
+
 function ToppingAdd() {
+  const navigate = useNavigate();
   const pic = () => {
     document.getElementById("pic").click();
   };
+
+  // alert
+  const [success, setSuccess] = useState(false);
+  const [fail, setFail] = useState(false);
 
   // store data
   const [form, setForm] = useState({
@@ -21,10 +28,6 @@ function ToppingAdd() {
 
   // set preview image
   const [preview, setPreview] = useState(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
 
   const handleChange = (e) => {
     setForm({
@@ -39,23 +42,98 @@ function ToppingAdd() {
     }
   };
 
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+
+      const config = {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      };
+
+      // Store form data as object
+      const formData = new FormData();
+      formData.set("title", form.title);
+      formData.set("price", form.price);
+      formData.set("thumbnail", form.thumbnail[0], form.thumbnail[0].name);
+
+      const response = await API.post("/product", formData, config);
+
+      if (response.data.status === "Success") {
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(false);
+          navigate("/");
+        }, 3000);
+      } else if (response.data.status === "Failed") {
+        setFail(true);
+        setTimeout(() => {
+          setFail(false);
+        }, 3000);
+      }
+    } catch (error) {
+      setFail(true);
+      setTimeout(() => {
+        setFail(false);
+      }, 4000);
+    }
+  };
+
   return (
     <>
       <div className={cssModules.bodyTopping}>
         <div className={cssModules.formContainer}>
           <h1>Add Topping</h1>
+
+          {success ? (
+            <h3
+              style={{
+                color: "green",
+                background: "#c5fce5",
+                textAlign: "center",
+                padding: "0.5rem 0",
+                fontSize: "1.15rem",
+                fontFamily: "avenirs",
+              }}
+            >
+              Add Topping Success
+            </h3>
+          ) : (
+            <></>
+          )}
+
+          {fail ? (
+            <h3
+              style={{
+                color: "red",
+                background: "#e0cecc",
+                textAlign: "center",
+                padding: "0.5rem 0",
+                fontSize: "1.15rem",
+                fontFamily: "avenirs",
+              }}
+            >
+              Add Topping Failed
+            </h3>
+          ) : (
+            <></>
+          )}
+
           <form onSubmit={handleSubmit}>
             <input
               type="text"
               name="title"
               placeholder="Topping Name"
               onChange={handleChange}
+              required
             />
             <input
               type="number"
               name="price"
               placeholder="Price"
               onChange={handleChange}
+              required
             />
             <input
               type="file"
