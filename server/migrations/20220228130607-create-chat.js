6fe4e26dfce166ca5008ac11184cdde835619e7c
Muhammad Zawawi <kembedt@gmail.com>
2022-02-28 20:17:01 +0700

    update

---
+++ server/migrations/20220228130607-create-chat.js
@@ -0,0 +1,48 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("chats", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      message: {
+        type: Sequelize.TEXT,
+      },
+      senderId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      recipientId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+        onUpdate: Sequelize.fn("NOW"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("chats");
+  },
+};
--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,9 +1,85 @@
-const socketIo = (io) => {
-  io.on("connection", (socket) => {
-    console.log("client connect:", socket.id);
+const { chat, user } = require("../../models");
 
-    socket.io("disconnect", () => {
-      console.log("client disconnect");
+const socketIo = (io) => {
+  io.on("connection", async (socket) => {
+    console.log("client connect: ", socket.id);
+
+    socket.on("load admin contact", async () => {
+      try {
+        const adminContact = await user.findOne({
+          include: [
+            {
+              model: profile,
+              as: "profile",
+              attributes: {
+                exclude: ["createdAt", "updatedAt"],
+              },
+            },
+          ],
+          where: {
+            status: "admin",
+          },
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "password"],
+          },
+        });
+
+        socket.emit("admin contact", adminContact);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    socket.on("load customer contacts", async () => {
+      try {
+        let customerContacts = await user.findAll({
+          include: [
+            {
+              model: profile,
+              as: "profile",
+              attributes: {
+                exclude: ["createdAt", "updatedAt"],
+              },
+            },
+            {
+              model: chat,
+              as: "recipientMessage",
+              attributes: {
+                exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"],
+              },
+            },
+            {
+              model: chat,
+              as: "senderMessage",
+              attributes: {
+                exclude: ["createdAt", "updatedAt", "idRecipient", "idSender"],
+              },
+            },
+          ],
+          attributes: {
+            exclude: ["createdAt", "updatedAt", "password"],
+          },
+        });
+
+        customerContacts = JSON.parse(JSON.stringify(customerContacts));
+        customerContacts = customerContacts.map((item) => ({
+          ...item,
+          profile: {
+            ...item.profile,
+            image: item.profile?.image
+              ? process.env.PATH_FILE + item.profile?.image
+              : null,
+          },
+        }));
+
+        socket.emit("customer contacts", customerContacts);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("client disconnected", socket.id);
     });
   });
 };
